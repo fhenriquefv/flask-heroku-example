06936803807ee6f64a93176691d0229bb4f9cada
hviana <hviana@tecgraf.puc-rio.br>
2020-04-07 19:35:18 -0300

    cross origin

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 """Flask App Project."""
 
 from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/brkga', methods=['POST'])
-@cross_origin()
 def brkga():
    data = request.get_json()	
    return jsonify(data)
 
 @app.route('/hello')
-@cross_origin()
 def index():
     """Return homepage."""
     json_data = {'oiee': 'flask!'}
